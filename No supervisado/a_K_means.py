c6215255305496f194821bcf598d2e1790347e2b
Juan Cruz Saldaño <70445613+fowardelcac@users.noreply.github.com>
2023-05-18 17:14:28 -0300

    Update a_K_means.py

--- No supervisado/a_K_means.py
+++ No supervisado/a_K_means.py
@@ -18,20 +18,16 @@ df = pd.DataFrame({'Feature 1': X[:, 0], 'Feature 2': X[:, 1], 'y': y})
 X = df[['Feature 1', 'Feature 2']].values.reshape((-1, 2))
 y = df.y
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 k = KMeans(n_clusters=3)
-k.fit(X_train, y_train)
-y_pred = k.predict(X_test)
+k.fit(X)
 
-rdo = pd.DataFrame({'F1': X_test[:, 0],
-                    'F2': X_test[:, 1],
-                    'y': y_test,
-                    'y_pred': y_pred
+rdo = pd.DataFrame({'F1': X[:, 0],
+                    'F2': X[:, 1],
+                    'y': k.labels_
                     })
 
 k.cluster_centers_
-sns.scatterplot(data = rdo, x='F1', y = 'F2', hue = 'y_pred')
+sns.scatterplot(data = rdo, x='F1', y = 'F2', hue = 'y')
 sns.scatterplot(x = k.cluster_centers_[:, 0], y = k.cluster_centers_[:, 1], label = 'Centroides')
 
 k.inertia_
